404e8a14d2e6074ead12e2cdec690f5a3e1c3845
Marcos <marcos.costa.2016a@gmail.com>
2024-11-05 17:28:29 +0000

    Added a script with usefull commands

---
+++ solrdata/ourStartup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script expects a container started with the following command.
+# docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate priProj
+
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' --data-binary "@./onePieceSchema.json" http://localhost:8983/solr/priProj/schema
+
+# Populate collection using mapped path inside container.
+docker exec -it meic_solr bin/post -c priProj /data/one_piece_data.json
+
+
+# command to delete a collection
+# docker exec meic_solr bin/solr delete -c priProj
+
+# command to create a collection
+# docker exec meic_solr bin/solr create -c "priProj"
