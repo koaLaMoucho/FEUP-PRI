5b1b70eab54d183a8ceef7d56472db14eb163a99
Marcos <marcos.costa.2016a@gmail.com>
2024-12-16 14:50:46 +0000

    Calculated qrels and results from hybrid

--- 07-semantic-search/solr/start_semantic.sh
+++ 07-semantic-search/solr/start_semantic.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # u might need to run "sed -i -e 's/\r$//' start_semantic.sh" to remove windows line endings and execute the script
 
-# This script expects a container started with the following command.
-# docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate priProj
+# This script expects a container started with the following command in this directory:
+# docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate simple
 
-# command to delete a collection
-docker exec meic_solr bin/solr delete -c priProj
+# create collections (simple already exists)
+docker exec meic_solr bin/solr create -c "complex"
+docker exec meic_solr bin/solr create -c "semantic"
 
-# command to create a collection
-docker exec meic_solr bin/solr create -c "priProj"
+# add synonyms for complex and semantic
+docker exec meic_solr cp /data/my_synonyms.txt /var/solr/data/complex/conf/
+docker exec meic_solr cp /data/my_synonyms.txt /var/solr/data/semantic/conf/
 
-# add synonyms file to solr config folder to be retrieved by schema
-docker exec meic_solr cp /data/my_synonyms.txt /var/solr/data/priProj/conf/
-
-# Schema definition via API
-curl -X POST -H 'Content-type:application/json' --data-binary "@./semantic_schema.json" http://localhost:8983/solr/priProj/schema
+# add schemas
+curl -X POST -H 'Content-type:application/json' --data-binary "@./simple_schema.json" http://localhost:8983/solr/simple/schema
+curl -X POST -H 'Content-type:application/json' --data-binary "@./complex_schema.json" http://localhost:8983/solr/complex/schema
+curl -X POST -H 'Content-type:application/json' --data-binary "@./semantic_schema.json" http://localhost:8983/solr/semantic/schema
 
 # Populate collection using mapped path inside container.
-docker exec -it meic_solr bin/post -c priProj /data/semantic_one_piece.json
+docker exec -it meic_solr bin/post -c simple /data/one_piece_data.json
+docker exec -it meic_solr bin/post -c complex /data/one_piece_data.json
+docker exec -it meic_solr bin/post -c semantic /data/semantic_one_piece.json
